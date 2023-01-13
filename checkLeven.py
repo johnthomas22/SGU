f73ff0424da9f69c431fd36e7132335e4c566421
Bruce Duncan <bwduncan@gmail.com>
2023-01-13 22:11:05 +0000

    Fixes
    - Indents should be consistent. 4 spaces is fine.
    - Use with statements with open so files are always closed afterwards.
    - Use f-strings
    - Use requests. It's better than raw urllib
    - Make try/excepts less broad

--- checkLeven.py
+++ checkLeven.py
@@ -1,67 +1,63 @@
 import json,  urllib.request, smtplib, pickle, os
+import requests
 from datetime import datetime
-#https://www2.sepa.org.uk/hydrodata/api/Level15/477620
 
-#
-#
-#
+URL = "https://www2.sepa.org.uk/hydrodata/api/Level15/477620"
+LIMIT = 1.4
+
 
 def main():
-    #open and read the file after the appending:
+    # open and read the file after the appending:
     try:
-       f = open("/home/john/lastreading.txt", "r")
-       result = float(f.read())
-    except:
-       result = 0
+        with open("/home/john/lastreading.txt", "r") as f:
+            result = float(f.read())
+    except Exception:
+        result = 0.0
 
     print("Last Result = ", result)
 
     file_name = '/home/john/checkLeven.pickled'
-    parameters = pickle.load(open(file_name, 'rb'))
+    with open(file_name, "rb") as pickle_f:
+        parameters = pickle.load(pickle_f)
 
     email_address_from = parameters['email_address_from']
     email_address_to = parameters['email_address_to']
     email_password = parameters['email_password']
 
-    print('email_address_from =', email_address_from)
-    print('email_address_to =', email_address_to)
+    print(f'{email_address_from=}')
+    print(f'{email_address_to=}')
     #print('email_password =', email_password)
 
-    with urllib.request.urlopen("https://www2.sepa.org.uk/hydrodata/api/Level15/477620") as url:
-        data = json.load(url)
+    data = requests.get(URL).json()
     last = data[-1]
-    #
-    # 2023-01-01T18:45:00
-    #
-    limit = 1.4
+
     lasttime = datetime.strptime(last["Timestamp"], "%Y-%m-%dT%H:%M:%S").timestamp()
     lasttimets = datetime.fromtimestamp(lasttime)
     lasttimestr = lasttimets.strftime("%d/%m/%Y %H:%M:%S")
 
-    if float(last["Value"]) > limit and float(last["Value"]) > result:
-       print('Increase detected...')
-       f = open("/home/john/lastreading.txt", "w")
-       f.write(last["Value"])
-       f.close()
-       with smtplib.SMTP_SSL('smtp.gmail.com', 465) as connection:
-        connection.login(email_address_from, email_password )
-        connection.sendmail(from_addr=email_address_from, to_addrs=email_address_to,
-        msg="subject:Loch Leven Sluice water level is RED \n\n SEPA's sensors indicated the water level is " + last["Value"] + " at " + lasttimestr + "\n Check https://www2.sepa.org.uk/WaterLevels/default.aspx?sd=t&lc=477620 for details.")
+    if float(last["Value"]) >= LIMIT and float(last["Value"]) > last_reading:
+        print('Increase detected...')
+        with open("/home/john/lastreading.txt", "w") as f:
+            f.write(last["Value"])
+        with smtplib.SMTP_SSL('smtp.gmail.com', 465) as connection:
+            connection.login(email_address_from, email_password )
+            connection.sendmail(from_addr=email_address_from, to_addrs=email_address_to,
+                msg=f"subject:Loch Leven Sluice water level is RED \n\n SEPA's sensors indicated the water level is {last['Value']} at {lasttimestr}\n Check https://www2.sepa.org.uk/WaterLevels/default.aspx?sd=t&lc=477620 for details.")
     else:
         print('Nothing to do here...')
 
-    if float(last["Value"]) < limit:
-       try:
-          os.remove("/home/john/lastreading.txt") 
-          print('Deleted last reading file')
-          with smtplib.SMTP_SSL('smtp.gmail.com', 465) as connection:
-              connection.login(email_address_from, email_password )
-              connection.sendmail(from_addr=email_address_from, to_addrs=email_address_to,
-              msg="subject:Loch Leven Sluice water level \n\n SEPA's sensors indicated the water level has fallen below RED level to " + last["Value"] + " at " + lasttimestr + "\n Check https://www2.sepa.org.uk/WaterLevels/default.aspx?sd=t&lc=477620 for details.")
-       except:
-           print("lastreading.txt not found")
+    if float(last["Value"]) < LIMIT:
+        try:
+            os.remove("/home/john/lastreading.txt")
+            print('Deleted last reading file')
+        except Exception:
+            print("lastreading.txt not found")
+        with smtplib.SMTP_SSL('smtp.gmail.com', 465) as connection:
+            connection.login(email_address_from, email_password )
+            connection.sendmail(from_addr=email_address_from, to_addrs=email_address_to,
+                msg=f"subject:Loch Leven Sluice water level \n\n SEPA's sensors indicated the water level has fallen below RED level to {last['Value']} at {lasttimestr}\n Check https://www2.sepa.org.uk/WaterLevels/default.aspx?sd=t&lc=477620 for details.")
 
-    print("SEPA's sensors indicated the water level is " + last["Value"] + " at " + lasttimestr + "\n Check https://www2.sepa.org.uk/WaterLevels/default.aspx?sd=t&lc=477620 for details.")
+    print(f"SEPA's sensors indicated the water level is {last['Value']} at {lasttimestr}\n Check https://www2.sepa.org.uk/WaterLevels/default.aspx?sd=t&lc=477620 for details.")
 
 if __name__ == "__main__":
     main()
